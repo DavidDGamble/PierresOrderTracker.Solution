8fc61df44d6b59019b0ea6eaea715b2a1ba90c27
DavidDGamble <daviddgamble@gmail.com>
2022-12-16 11:15:09 -0800

    Add HomeController.cs and Home folder in Views with Index.cshtml.

---
+++ PierresOrderTracker/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PierresOrderTracker.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
